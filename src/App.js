ef21485f3c996f966443f0acf50074e92c49977e
mehdispr <najikarim1234@gmail.com>
2019-06-27 15:52:29 +0100

    pages folder added

--- src/App.js
+++ src/App.js
@@ -1,51 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
-import Nav from "./components/Nav/Nav";
-import Top5 from './components/Top5/Top5';
-import NewIn from './components/newProj/NewIn';
-import Contact from './components/Contact/Contact';
-import Footer from "./components/Footer/Footer";
-import AboutUs from "./components/AboutUs/AboutUs";
+import HomePage from "./components/Ppages/HomePage/HomePage";
 
 function App() {
   return (
-    <div>
-      <Nav/>
-      <Header/>
-        <section className="page-section" id="topDemandes">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12 text-center">
-                <h2 className="section-heading text-uppercase">Top demandes</h2>
-                <h3 className="section-subheading text-muted">Les demandes les plus visitées</h3>
-              </div>
-            </div>
-            
-            <div>
-                <Top5/>
-            </div>
-            </div>
-        </section>
-
-        <section className="page-section" id="newIn">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12 text-center">
-                <h2 className="section-heading text-uppercase">Nouvelles demandes</h2>
-                <h3 className="section-subheading text-muted">Les demandes qui vient d'être créé</h3>
-              </div>
-            </div>
-            
-            <div>
-                <NewIn/>
-            </div>
-            </div>
-        </section>
-        <AboutUs/>
-        <Contact/>
-        <Footer/>
-    </div>
+      <HomePage/>
   );
 }
 
---
+++ src/components/Ppages/HomePage/HomePage.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import Header from '../../Header/Header';
+import Nav from "../../Nav/Nav";
+import Top5 from '../../Top5/Top5';
+import NewIn from '../../newProj/NewIn';
+import Contact from '../../Contact/Contact';
+import Footer from "../../Footer/Footer";
+import AboutUs from '../../AboutUs/AboutUs';
+
+class HomePage extends Component {
+    render() { 
+        return ( 
+            <React.Fragment>
+                <Nav/>
+                <Header/>
+                <section className="page-section" id="topDemandes">
+                    <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12 text-center">
+                        <h2 className="section-heading text-uppercase">Top demandes</h2>
+                        <h3 className="section-subheading text-muted">Les demandes les plus visitées</h3>
+                        </div>
+                    </div>
+                    
+                    <div>
+                        <Top5/>
+                    </div>
+                    </div>
+                </section>
+
+                <section className="page-section" id="newIn">
+                    <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12 text-center">
+                        <h2 className="section-heading text-uppercase">Nouvelles demandes</h2>
+                        <h3 className="section-subheading text-muted">Les demandes qui vient d'être créé</h3>
+                        </div>
+                    </div>
+                    
+                    <div>
+                        <NewIn/>
+                    </div>
+                    </div>
+                </section>
+                
+                <AboutUs/>
+                <Contact/>
+                <Footer/>
+            </React.Fragment>
+         );
+    }
+}
+ 
+export default HomePage;
